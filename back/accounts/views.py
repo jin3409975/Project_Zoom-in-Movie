c2e390a7ec9d22900e7447167a06fb1badbf72c6
JIEUN LEE <jin3409975@gmail.com>
2023-11-17 14:20:41 +0900

    [ADD] back > accounts > views.py > signup 함수 생성

--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,11 +1,42 @@
+from rest_framework.response import Response
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.http import require_http_methods, require_POST
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
 
 # Create your views here.
+
+@api_view(['POST'])
+def signup(request):
+    # Client 에서 보내온 정보 받기
+    password = request.data.get('password')
+    passwordConfirmation = request.data.get('passwordConfirmation')
+    # 비밀번호 일치 여부 확인
+    if password != passwordConfirmation:
+        return Response({ 'error': '비밀번호가 일치하지 않습니다.'})
+
+    # 사용자가 보낸 데이터로 UserSerializer 를 통해 데이터 생성
+    else:
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            # 그냥 저장하고 끝내면 비밀번호 유출
+            user = serializer.save()
+
+            # 비밀번호 해싱
+            user.set_password(request.data.get('password'))
+            user.save()
+
+            return Response(serializer.data)
+
+
 
 @require_http_methods(['GET', 'POST'])   # GET 또는 POST로만 호출 가능
 def login(request):
@@ -20,6 +51,7 @@ def login(request):
         'form': form
     }
     return render(request, 'accounts/login.html', context)
+
 
 
 @require_POST
