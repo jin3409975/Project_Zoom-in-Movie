cfdd3722109154b1ff4a4b6536fc096ba30a49e8
JIEUN LEE <jin3409975@gmail.com>
2023-11-17 13:28:21 +0900

    [ADD] back > accounts > views.py 로그인 함수 생성

--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import login as auth_login
+
 
 # Create your views here.
+
+@require_http_methods(['GET', 'POST'])   # GET 또는 POST로만 호출 가능
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:   # GET
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
