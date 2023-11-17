701c1f65994cff8329a3d50b6ecbf469b0a20c9e
JIEUN LEE <jin3409975@gmail.com>
2023-11-17 13:36:19 +0900

    [UPDATE] back > accounts > views.py 로그아웃 함수 설정

--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_POST
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 # Create your views here.
@@ -12,10 +13,16 @@ def login(request):
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
             auth_login(request, form.get_user())
-            return redirect('movies:index')
+            return redirect('articles:index')
     else:   # GET
         form = AuthenticationForm()
     context = {
         'form': form
     }
     return render(request, 'accounts/login.html', context)
+
+
+@require_POST
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
