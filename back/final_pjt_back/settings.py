97b22bf64cdd253c6da46abef6c65448eff66d24
cha <ckguscjf03@gmail.com>
2023-11-17 11:39:08 +0900

    [UPDATE]: back > movies > models.py, urls.py, settings.py

--- back/final_pjt_back/settings.py
+++ back/final_pjt_back/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'final_pjt_back.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
