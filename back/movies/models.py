f344ed5292111cba261aca5c40e73ca5a62b15cc
cha <ckguscjf03@gmail.com>
2023-11-17 12:56:30 +0900

    [UPDATE]: movies > models.py

--- back/movies/models.py
+++ back/movies/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 from django.conf import settings
-from django.core.validators import MaxValueValidator, MinValueValidator
-# from ..accounts
+
 
 class Genre(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)\
+    
     
 class Actor(models.Model):
     name = models.CharField(max_length=255)
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
     release_date = models.DateTimeField()
+    content = models.TextField()
+    rating = models.FloatField()
+    poster_path = models.CharField(max_length=200)
     like_users = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name='like_movies'
     )
-    rating = models.IntegerField(
-        default=5,
-        validators=[
-            MaxValueValidator(10),
-            MinValueValidator(1)
-        ]
+    genre = models.ManyToManyField(
+        Genre, related_name='genre_movie'
     )
-    image = models.CharField(max_length=255)
+    actors = models.ManyToManyField(
+        Actor, related_name='actor_movie'
+    )
 
 
 class Comment(models.Model):
-    # content = var
+    content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
