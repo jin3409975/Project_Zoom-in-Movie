ab58c694048d901ebbe2978371cde35dbc855bbb
JIEUN LEE <jin3409975@gmail.com>
2023-11-18 17:08:17 +0900

    [ADD] users_info 함수 생성

--- back/movies/views.py
+++ back/movies/views.py
@@ -3,9 +3,12 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.contrib.auth import get_user_model
 from django.conf import settings
 from .models import Movie, Comment, Genre, Actor
 from .serializers import MovieListSerializer, CommentListSerializer
+from accounts.serializers import UserSerializer
+
 
 
 @api_view(['GET', 'POST'])
@@ -72,3 +75,32 @@ def movie_like(request, my_pk, movie_id):
       liking = True
   
   return Response(liking)
+
+
+@api_view(['POST'])
+def my_movie_like(request, my_pk):
+  me = get_object_or_404(get_user_model(), pk=my_pk)
+  data = []
+  movies = me.like_movies.all()
+  for movie in movies:
+    movie = get_object_or_404(Movie, pk=movie.id)
+    serializer = MovieSerializer(movie)
+    data.append(serializer.data)
+  
+  return Response(data)
+
+
+
+@api_view(['POST'])
+def users_info(request):
+  users = request.data.get('users')
+  movies = []
+  for user in users:
+      user = get_object_or_404(get_user_model(), pk=user)
+      serializer = UserSerializer(user)
+      like_movies = serializer.data.get('like_movies')
+      for movie in like_movies:
+          if movie not in movies:
+              movies.append(movie)
+  
+  return Response(movies)
