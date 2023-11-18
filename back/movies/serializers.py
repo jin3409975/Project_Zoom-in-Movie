d7dba41d5a10dd7dd153bc504fcc315d8a6eb1d1
JIEUN LEE <jin3409975@gmail.com>
2023-11-18 22:56:06 +0900

    [ADD] MovieSerializer

--- back/movies/serializers.py
+++ back/movies/serializers.py
@@ -5,6 +5,15 @@ from .models import Movie, Comment, Genre, Actor
 class MovieListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
+        fields = ('id', 'movie_id', 'title', 'original_title', 
+                  'overview', 'rating', 'release_date', 'runtime',
+                  'popularity', 'adult', 'genres', 'actors',
+                  'like_users',)
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
         fields = '__all__'
         read_only_fields = ('like_movies', 'like_users',)
         
