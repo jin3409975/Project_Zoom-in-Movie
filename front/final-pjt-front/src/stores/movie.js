76e2e7b6ce8f7e70988052c9d55bbc96d86f565d
Bright <ckguscjf03@gmail.com>
2023-11-20 21:28:58 +0900

    test

---
+++ front/final-pjt-front/src/stores/movie.js
@@ -0,0 +1,36 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
+
+export const useCounterStore = defineStore('counter', () => {
+  const movies = ref([])
+  const router = useRouter()
+  const API_URL = 'http://127.0.0.1:8000'
+  const token = ref(null)
+  const isLogin = computed(() => {
+    if (token.value === null) {
+      return false
+    } else {
+      return true
+    }
+  })
+
+
+  // DRF에 article 조회 요청을 보내는 action
+  const getMovies = function () {
+    axios({
+      method: 'get',
+      url: `${API_URL}/api/v1/movies/`
+    })
+      .then((res) =>{
+        console.log(res)
+        movies.value = res.data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  return { movies, API_URL, isLogin, token, getMovies }
+}, { persist: true })
