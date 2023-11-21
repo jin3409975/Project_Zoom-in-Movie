cd6ebba3bde334e36c8ba083625b85227ee875a4
cha <ckguscjf03@gmail.com>
2023-11-21 17:47:04 +0900

    [UPDATE] detail

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "accounts"
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('user/<int:userId>/', views.user),
+    path('user/<int:userId>/', views.get_user),
 ]
