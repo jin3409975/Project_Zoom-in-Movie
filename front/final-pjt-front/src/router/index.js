1cb4f8311226bd3c79597e7ed21de39c6344d8f8
JIEUN LEE <jin3409975@gmail.com>
2023-11-22 16:12:42 +0900

    [ADD] 사용자 접근 제한

--- front/final-pjt-front/src/router/index.js
+++ front/final-pjt-front/src/router/index.js
@@ -4,7 +4,6 @@ import CategoryView from '@/views/CategoryView.vue'
 import SignUpView from '@/views/SignUpView.vue'
 import MovieDetailView from '@/views/MovieDetailView.vue'
 import LoginView from '@/views/LoginView.vue'
-import LogoutView from '@/views/LogoutView.vue'
 import MyPageView from '@/views/MyPageView.vue'
 import RecommendChoiceView from '@/views/RecommendChoiceView.vue'
 import RecommendView from '@/views/RecommendView.vue'
@@ -56,6 +55,23 @@ const router = createRouter({
   scrollBehavior() {
     // 페이지 전환 시 스크롤을 맨 위로 이동
     return { top: 0 }
+  }
+})
+
+
+import { useCounterStore } from '@/stores/account'
+
+router.beforeEach((to, from) => {
+  const store = useCounterStore()
+  if ((to.name === 'CategoryView'||
+    to.name === 'RecommendView'||
+    to.name === 'MovieDetailView') && !store.isLogin) {
+    window.alert('로그인이 필요합니다.')
+    return { name: 'LoginView' }
+  }
+  if ((to.name === 'SignUpView' || to.name === 'LoginView') && (store.isLogin)) {
+    window.alert('이미 로그인 했습니다.')
+    return { name: 'MainView' }
   }
 })
 
