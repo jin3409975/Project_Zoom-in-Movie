9607cc9390be0a29808bc077466ed8e722e0eb52
Bright <ckguscjf03@gmail.com>
2023-11-23 00:18:08 +0900

    [ADD] Typed.js

---
+++ front/final-pjt-front/src/components/Typed.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-lg-8 text-center mx-auto my-auto">
+      <h1 class="text-white">
+        "<span class="text-white" id="typed"></span>"
+      </h1>
+      <div id="typed-strings">
+        <h1>team</h1>
+      </div>
+    </div>
+</template>
+
+<script setup>
+import Typed from "typed.js";
+
+
+onMounted(() => {
+  if (document.getElementById("typed")) {
+    // eslint-disable-next-line no-unused-vars
+    const typed = new Typed("#typed", {
+      stringsElement: "#typed-strings",
+      typeSpeed: 90,
+      backSpeed: 90,
+      backDelay: 200,
+      startDelay: 500,
+      loop: true,
+    });
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
